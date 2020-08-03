9138f8ffaa92b2e73a058ba9e97682f46622f567
willfeliz1 <william_mfelizardo@hotmail.com>
2020-08-03 15:37:36 -0300

    feat: created calendar using dateTimePicker lib

--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { FlatList } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { Provider } from './index';
 
 interface ProviderContainerProps {
@@ -17,6 +18,7 @@ export const Container = styled.View`
 
 export const Header = styled.View`
   padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
   background: #28262e;
 
   flex-direction: row;
@@ -68,4 +70,27 @@ export const ProviderName = styled.Text<ProviderNameProps>`
   font-family: 'Roboto-Medium';
   font-size: 16px;
   color: ${(props) => (props.selected ? '#232129' : '#f4ede8')};
+`;
+
+export const Calendar = styled.View``;
+
+export const Title = styled.Text`
+  font-family: 'Roboto-Medium';
+  color: #f4ede8;
+  font-size: 24px;
+  margin: 0 24px 24px;
+`;
+
+export const OpenDatePickerButton = styled(RectButton)`
+  height: 46px;
+  background: #ff9000;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+  margin: 0 24px;
+`;
+export const OpenDatePickerButtonText = styled.Text`
+  font-family: 'Roboto-Medium';
+  font-size: 16px;
+  color: #232129;
 `;
